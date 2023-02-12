6c77caf38dc21aabdc5115753f3a71b478e4b5df
emineealtuntas <111460539+emineealtuntas@users.noreply.github.com>
2023-02-12 21:04:50 +0300

    dashboard işlemlerine kadar geldim

--- Core_Proje/Controllers/AdminController.cs
+++ Core_Proje/Controllers/AdminController.cs
@@ -28,6 +28,10 @@ namespace Core_Proje.Controllers
         {
             return PartialView();
         }
+        public PartialViewResult NewSidebar()
+        {
+            return PartialView();
+        }
 
     }
 }
